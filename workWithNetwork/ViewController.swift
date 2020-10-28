73ca9da1ab3077248d88599f3730852b38ff171a
Damir Lutfullin <dmr.lutfullin@gmail.com>
2020-10-28 08:04:22 +0500

    download image

--- workWithNetwork/ViewController.swift
+++ workWithNetwork/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageForDownload: UIImageView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        activityIndicator.hidesWhenStopped = true
+        imageForDownload.isHidden = true
+        activityIndicator.startAnimating()
+        downloadImage()
     }
-
-
+    
+    func downloadImage() {
+        guard let url = URL(string:"https://upload.wikimedia.org/wikipedia/commons/4/42/Blue_sky%2C_white-gray_clouds.JPG") else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data, let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async {
+                self.imageForDownload.image = image
+                self.imageForDownload.isHidden = false
+                self.activityIndicator.stopAnimating()
+            }
+        }.resume()
+    }
 }
 
---
+++ workWithNetwork/firstViewController.swift
@@ -0,0 +1,14 @@
+//
+//  firstViewController.swift
+//  workWithNetwork
+//
+//  Created by Damir Lutfullin on 28.10.2020.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    
+    
+}
