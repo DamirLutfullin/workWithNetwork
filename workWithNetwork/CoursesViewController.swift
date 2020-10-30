a195f892a5f93f265afa40e389ee189500024069
Damir Lutfullin <dmr.lutfullin@gmail.com>
2020-10-30 14:13:14 +0500

    get and parse data

--- workWithNetwork/CoursesViewController.swift
+++ workWithNetwork/CoursesViewController.swift
@@ -11,6 +11,8 @@ class CoursesViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
     
+    private var courses = [Course]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
@@ -18,23 +20,56 @@ class CoursesViewController: UIViewController {
         fetchData()
     }
     
-    func fetchData() {
-//        let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_course"
+    private func fetchData() {
+        //        let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_course"
         let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_courses"
-//        let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_website_description"
-//        let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_missing_or_wrong_fields"
+        //        let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_website_description"
+        //        let jsonUrlString = "https://swiftbook.ru//wp-content/uploads/api/api_missing_or_wrong_fields"
         
         guard let url = URL(string: jsonUrlString) else { return }
-        URLSession.shared.dataTask(with: url) { (data, _, _) in
+        URLSession.shared.dataTask(with: url) { [self] (data, _, _) in
             guard let data = data else { return }
             do {
-                let course = try JSONDecoder().decode(Course.self, from: data)
-                print(course)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                self.courses = try decoder.decode([Course].self, from: data)
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             } catch let error {
                 print(error, error.localizedDescription)
             }
         }.resume()
     }
+    
+    private func configureCell(cell: TableViewCell, for indexPath: IndexPath) {
+        
+        let course = courses[indexPath.row]
+        cell.courseNameLabel.text = course.name
+        
+        if let numberOfLessons = course.numberOfLessons {
+            cell.numberOfLessons.text = "Number of lessons: \(numberOfLessons)"
+        }
+        
+        if let numberOfTests = course.numberOfTests {
+            cell.numberOfTests.text = "Number of tests: \(numberOfTests)"
+        }
+        
+        //работа с данными ассинхронно в глобальном потоке
+        DispatchQueue.global().async {
+            guard
+                let stringUrl = course.imageUrl,
+                let imageUrl = URL(string: stringUrl),
+                let imageData = try? Data(contentsOf: imageUrl)
+            else { return }
+            
+            // радота с вью ассинхронно в главном потоке
+            DispatchQueue.main.async {
+                cell.courseImage.image = UIImage(data: imageData)
+            }
+        }
+    }
+    
 }
 
 extension CoursesViewController: UITableViewDelegate {
@@ -44,11 +79,12 @@ extension CoursesViewController: UITableViewDelegate {
 extension CoursesViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        0
+        courses.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        configureCell(cell: cell, for: indexPath)
         return cell
     }
 }
--- workWithNetwork/TableViewCell.swift
+++ workWithNetwork/TableViewCell.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
-
     @IBOutlet var courseImage: UIImageView!
     @IBOutlet var courseNameLabel: UILabel!
     @IBOutlet var numberOfLessons: UILabel!
